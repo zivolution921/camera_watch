a2c9432a15b2277509cb6b562c2df8cca62e7c6a
Ziv Zamechek <ziv921@yahoo.com>
2015-12-27 15:06:47 -0500

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,14 @@ class ContactsController < ApplicationController
 
 	def create
     @contact = Contact.new(contact_params)
+
     if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+
+      ContactMailer.contact_email(name, email, body).deliver
+
     	flash[:success] = "Message sent."
       redirect_to new_contact_path
     else
@@ -13,6 +20,7 @@ class ContactsController < ApplicationController
       redirect_to new_contact_path
     end
   end
+  
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -18,7 +18,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private 
   def select_plan
     unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
-      flash[:notice] = "PLease select a membership plan to sign up"
+      flash[:notice] = "Please select a membership plan to sign up"
       redirect_to root_url
     end
   end
